27bd30c9411d01354d0b0391359167c20b648683
distialm <distialmira@gmail.com>
2026-02-03 08:58:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validatelogin() {
+  let user = document.forms['login']['username'].value;
+  let pass = document.getElementById('password').value;
+
+  if (user === "admin" && pass === "123") {
+    alert("Welcome");
+    
+    // pindah ke halaman index.html
+    window.location.href = "home.html";
+    return false; // supaya form tidak reload
+  } else {
+    alert("Username atau password salah");
+    return false;
+  }
+}
